1c5fc5f2c4716e2ed3d8859fe117787bde5c349b
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2023-09-12 16:54:27 +0530

    Update config.py

--- SpotBot/config.py
+++ SpotBot/config.py
@@ -1,11 +1,11 @@
 class Configs(object):
-    API_KEY = '5351309281:AAH9tBdIyJk20PvgTGTsKji2dX1oA8ocnks'  # Telegram Bot TOKEN
-    API_ID = '99389'  # Obtain from my.telegram.org
-    API_HASH = '1a0da7a16603e77bf269a6bf7f703b77'  # Obtain from my.telegram.org
+    API_KEY = '6033460350:AAEid4RRMYPTL53HzNfu2uLYOZKLFvMdFjE'  # Telegram Bot TOKEN
+    API_ID = '25377875'  # Obtain from my.telegram.org
+    API_HASH = 'cf80e342be48570ca2e4c9d2c7695413'  # Obtain from my.telegram.org
     CLIENT_ID = '681e1633e00342868821ba5d6516883b'  # Spotify Client ID
     CLIENT_SECRET = '75f2371108394348a015fb377d781a2a'  # Spotify Client Secret
     BOT_NAME = 'SpotSave'  # Bot Title Ex: SpotSave
     BOT_UNAME = 'spotsavebot'  # Bot Username without @
-    OWNER = '@Incognito_Coder'  # Developer/Owner @username
-    CHANNEL = 'ic_mods'  # Channel Username without @
-    CHANNEL_URL = 'https://t.me/ic_mods'  # Channel link https://t.me/ic_mods
+    OWNER = '@EMPERORns'  # Developer/Owner @username
+    CHANNEL = 'psycobotz'  # Channel Username without @
+    CHANNEL_URL = 'https://t.me/psycobotz'  # Channel link https://t.me/ic_mods
