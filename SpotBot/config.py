3a28fb0a480467e5cbaf27a066f51d696dba3a5f
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2023-09-12 17:05:40 +0530

    Update config.py

--- SpotBot/config.py
+++ SpotBot/config.py
@@ -2,8 +2,8 @@ class Configs(object):
     API_KEY = '6033460350:AAEid4RRMYPTL53HzNfu2uLYOZKLFvMdFjE'  # Telegram Bot TOKEN
     API_ID = '25377875'  # Obtain from my.telegram.org
     API_HASH = 'cf80e342be48570ca2e4c9d2c7695413'  # Obtain from my.telegram.org
-    CLIENT_ID = '681e1633e00342868821ba5d6516883b'  # Spotify Client ID
-    CLIENT_SECRET = '75f2371108394348a015fb377d781a2a'  # Spotify Client Secret
+    CLIENT_ID = 'a697d56774d040c587d0adbbcd07e965'  # Spotify Client ID
+    CLIENT_SECRET = '6253234360ab4f47866c709509284425'  # Spotify Client Secret
     BOT_NAME = 'SpotSave'  # Bot Title Ex: SpotSave
     BOT_UNAME = 'spotsavebot'  # Bot Username without @
     OWNER = '@EMPERORns'  # Developer/Owner @username
